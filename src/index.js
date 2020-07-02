fee58d7311003570a31ecc231be02aef044a56d4
Rodion Mescheryakov <rodion.mescheryakov@oracle.com>
2020-07-02 16:23:05 +0300

    Day 1. Tree of components

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const movie = {
+  title: 'Avengers: Infinity War',
+  vote_average: 8.5,
+  image: 'https://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg',
+  overview:
+    'As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.',
+};
+
+function Image(props) {
+  return <img src={props.src} alt={props.alt} />;
+}
+
+class MovieItem extends React.Component {
+  render() {
+    const {
+      data: { title, vote_average, image },
+    } = this.props;
+
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem data={movie} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
