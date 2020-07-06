a5754ee3bc89c3ab33c7f817386baee1f66bd1dd
Rodion Mescheryakov <rodion.mescheryakov@oracle.com>
2020-07-06 17:52:57 +0300

    Day 5. Movie card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ class App extends React.Component {
 
     this.state = {
       movies: moviesData,
+      moviesWillWatch: [],
     };
   }
 
@@ -19,12 +20,35 @@ class App extends React.Component {
     });
   };
 
+  addMovieToWillWatch = (movie) => {
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch,
+    });
+  };
+
   render() {
     return (
-      <div>
-        {this.state.movies.map((movie) => (
-          <MovieItem key={movie.id} movie={movie} removeMovie={this.removeMovie} />
-        ))}
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <div className="row">
+              {this.state.movies.map((movie) => (
+                <div className="col-6 mb-4" key={movie.id}>
+                  <MovieItem
+                    movie={movie}
+                    removeMovie={this.removeMovie}
+                    addMovieToWillWatch={this.addMovieToWillWatch}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-3">
+            <p>Will watch: {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 
-const MovieItem = (props) => {
+const MovieItem = ({ movie, removeMovie, addMovieToWillWatch }) => {
   return (
     <div>
-      <p>{props.movie.title}</p>
-      <button
-        onClick={() => {
-          props.removeMovie(props.movie);
-        }}
-      >
-        Delete movie
-      </button>
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_movie || movie.poster_path}`}
+          alt=""
+        />
+      </div>
+
+      <div className="card-body">
+        <h6 className="card-title">{movie.title}</h6>
+
+        <div className="d-flex justify-content-between align-items">
+          <p className="mb-0">Rating: {movie.vote_average}</p>
+
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => {
+              addMovieToWillWatch(movie);
+            }}
+          >
+            Will watch
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              removeMovie(movie);
+            }}
+          >
+            Delete movie
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
