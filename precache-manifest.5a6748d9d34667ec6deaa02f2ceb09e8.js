a7ccad76db7d557dc99931296fa85041c7025b69
Rodion Mescheryakov <rodion.mescheryakov@oracle.com>
2020-07-12 14:35:41 +0300

    Updates

---
+++ precache-manifest.5a6748d9d34667ec6deaa02f2ceb09e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee6db26056eb55f8b8422ca156fb16e",
+    "url": "/reactwarriors-master-class/index.html"
+  },
+  {
+    "revision": "a8d6e90ab42435484c99",
+    "url": "/reactwarriors-master-class/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "64ca58b41053a6d1238c",
+    "url": "/reactwarriors-master-class/static/css/main.8de4a7c3.chunk.css"
+  },
+  {
+    "revision": "a8d6e90ab42435484c99",
+    "url": "/reactwarriors-master-class/static/js/2.c396b54a.chunk.js"
+  },
+  {
+    "revision": "64ca58b41053a6d1238c",
+    "url": "/reactwarriors-master-class/static/js/main.3bf4097d.chunk.js"
+  },
+  {
+    "revision": "c34383b7bf6601d1b35d",
+    "url": "/reactwarriors-master-class/static/js/runtime~main.199926b5.js"
+  }
+]);
